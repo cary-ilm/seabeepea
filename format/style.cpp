598322cbca275ed3b5c444e5ff2c45d7a170657b
Cary Phillips <seabeepea@gmail.com>
2020-07-27 10:48:53 -0700

    clang-format
    Signed-off-by: Cary Phillips <seabeepea@gmail.com>

---
+++ format/style.cpp
@@ -0,0 +1,26 @@
+
+
+class Foo 
+{
+    Foo (int x) 
+    {
+    }
+
+    virtual ~Foo() = 0;
+
+    int x;
+    float y;
+}
+    ;
+
+
+Foo::Foo (int x)
+{
+    if (a < b) x = 1;
+
+    for (int i=0; i<3; i++)
+        x++;
+}
+
+
+    
