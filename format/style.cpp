d32a6f69b8beca5355bdf58312e1a1bd4092a4a4
Cary Phillips <seabeepea@gmail.com>
2020-07-27 18:35:50 -0700

    draft #2
    Signed-off-by: Cary Phillips <seabeepea@gmail.com>

--- format/style.cpp
+++ format/style.cpp
@@ -1,26 +1,57 @@
 
-
+namespace ABC {
+    
 class Foo 
 {
-    Foo (int x) 
-    {
-    }
+public:
+    Foo (int z):x(x),y(0)
+ {
+ }
 
-    virtual ~Foo() = 0;
+ virtual ~Foo() = 0;
 
-    int x;
-    float y;
+ int operator+=(const Foo& other);
+private:
+ int x;
+ float y;
 }
-    ;
+ ;
 
+template <typename T> T foo() {}
+
+template <typename T> T foo(int aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, int bbbbbbbbbbbbbbbbbbbbbbb) {}
+    
 
 Foo::Foo (int x)
 {
-    if (a < b) x = 1;
+ if (a < b) x = 1;
 
-    for (int i=0; i<3; i++)
-        x++;
+ x = a < b ? (y + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9) : (z + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+ 
+ 
+ for (int i=0; i<3; i++)
+ x++;
+
+ for (int j=0; j<foo(); j+=1) { bar(x); foo_bar ();
+ }
+
+// case (x) {
+//  1:
+//  foo();
+//  break;
+//  2: bar(x);
+//  break;
+//  }
+
+switch (x) {
+ case 1:
+ foo();
+ break;
+ case 2: bar(x);
+ break;
+ }
 }
 
 
-    
+ 
+}
